e978127c499aa91f38953ec799bcda33ee8cb9c0
João Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2024-10-04 05:43:09 -0300

    fix: Change directory in every step

--- compare_readme.py
+++ compare_readme.py
@@ -6,16 +6,10 @@ from md_template import md_template
 load_dotenv()
 github_token = os.getenv('GITHUB_TOKEN')
 
-# get the current working directory
-current_working_directory = os.getcwd()
-
-# print output to the console
-print(current_working_directory)
-
-# response = requests.get("https://api.github.com/orgs/jerobas/repos", headers={'authorization': f'{github_token}'})
-# repos = response.json()
+response = requests.get("https://api.github.com/orgs/jerobas/repos", headers={'authorization': f'{github_token}'})
+repos = response.json()
     
-# string = md_template(repos)
+string = md_template(repos)
 
-# with open("../.github/profile/readme.md", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
-#     output_file.write(string)
+with open("../.github/profile/readme.md", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+    output_file.write(string)
