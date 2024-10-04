5bc0e421afb50cda550e40bf5c689b4d7913bfb7
João Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2024-10-04 05:37:01 -0300

    test: Printing cwd

--- compare_readme.py
+++ compare_readme.py
@@ -6,13 +6,16 @@ from md_template import md_template
 load_dotenv()
 github_token = os.getenv('GITHUB_TOKEN')
 
-response = requests.get("https://api.github.com/orgs/jerobas/repos", headers={'authorization': f'{github_token}'})
-repos = response.json()
+# get the current working directory
+current_working_directory = os.getcwd()
 
-for i in repos:
-    print(i['default_branch'])
+# print output to the console
+print(current_working_directory)
+
+# response = requests.get("https://api.github.com/orgs/jerobas/repos", headers={'authorization': f'{github_token}'})
+# repos = response.json()
     
-string = md_template(repos)
+# string = md_template(repos)
 
-with open("../.github/profile/readme.md", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
-    output_file.write(string)
+# with open("../.github/profile/readme.md", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+#     output_file.write(string)
