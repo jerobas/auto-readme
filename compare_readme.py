f2b03b3e81208aa3a660113563a3399b4c1ae587
João Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2024-10-04 05:06:51 -0300

    fix: Changes file structure on actions

--- compare_readme.py
+++ compare_readme.py
@@ -6,7 +6,7 @@ from md_template import md_template
 load_dotenv()
 github_token = os.getenv('GITHUB_TOKEN')
 
-response = requests.get("https://api.github.com/orgs/jerobas/repos", headers={'authorization': f'{github_token}'})
+response = requests.get("https://apiglobal-repo/.github.com/orgs/jerobas/repos", headers={'authorization': f'{github_token}'})
 repos = response.json()
 
 for i in repos:
@@ -14,5 +14,5 @@ for i in repos:
     
 string = md_template(repos)
 
-with open("../.github/profile/readme.md", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+with open("../global-repo/.github/profile/readme.md", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
     output_file.write(string)
